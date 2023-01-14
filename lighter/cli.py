d63e56d2aff81b02962b8ccf0166fcef011e77db
Ibrahim Hadzic <ibrahimhadzic45@gmail.com>
2023-01-14 01:32:33 -0500

    switch to monai bundle config

--- lighter/cli.py
+++ lighter/cli.py
@@ -1,50 +1,79 @@
-import click
+import fire
+import yaml 
 
-from lighter.runner import run
+from monai.bundle.scripts import run
 
 
-# Interface
-@click.group()
 def interface():
-    """Lighter - config-based wrapper for PyTorch Lightning for
-    deep learning with little to no coding required.
+    fire.Fire({
+        "fit": fit,
+        "validate": validate,
+        "predict": predict,
+        "test": test,
+        "tune": tune
+    })
+
+def fit(**kwargs):
+    fit_config = """
+        fit:
+            _method_: >
+                $@trainer.fit(model=@fit#model,
+                            ckpt_path=@fit#ckpt_path)
+            model: "@system"
+            ckpt_path: null
     """
+    run("fit", **yaml.safe_load(fit_config), **kwargs)
 
+def validate(**kwargs):
+    validate_config = """
+        validate:
+            _method_: > 
+                $@trainer.validate(model=@validate#model,
+                                ckpt_path=@validate#ckpt_path,
+                                verbose=@validate#verbose)
+            model: "@system"
+            ckpt_path: null
+            verbose: True
+    """
+    run("validate", **yaml.safe_load(validate_config), **kwargs)
 
-# Train
-@interface.command()
-@click.argument("args", nargs=-1)
-def train(args):
-    run("fit", args)
+def predict(**kwargs):
+    predict_config = """
+        predict:
+            _method_: >
+                $@trainer.predict(model=@predict#model,
+                                ckpt_path=@predict#ckpt_path)
+            model: "@system"
+            ckpt_path: null
+    """
+    run("predict", **yaml.safe_load(predict_config), **kwargs)
 
+def test(**kwargs):
+    test_config = """
+        test:
+            _method_: >
+                $@trainer.test(model=@test#model,
+                            ckpt_path=@test#ckpt_path,
+                            verbose=@test#verbose)
+            model: "@system"
+            ckpt_path: null
+            verbose: True
+    """
+    run("test", **yaml.safe_load(test_config), **kwargs)
 
-# Validate
-@interface.command()
-@click.argument("args", nargs=-1)
-def validate(args):
-    run("validate", args)
-
-
-# Predict
-@interface.command()
-@click.argument("args", nargs=-1)
-def predict(args):
-    run("predict", args)
-
-
-# Test
-@interface.command()
-@click.argument("args", nargs=-1)
-def test(args):
-    run("test", args)
-
-
-# Tune
-@interface.command()
-@click.argument("args", nargs=-1)
-def tune(args):
-    run("tune", args)
-
-
-if __name__ == "__main__":
-    interface()
+def tune(**kwargs):
+    tune_config = """
+        tune:
+            _method_: > 
+                $@trainer.tune(model=@tune#model,
+                            ckpt_path=@tune#ckpt_path,
+                            scale_batch_size_kwargs=@tune#scale_batch_size_kwargs,
+                            lr_find_kwargs=@tune#lr_find_kwargs,
+                            method=@tune#method)
+            model: "@system"
+            ckpt_path: null
+            scale_batch_size_kwargs: null
+            lr_find_kwargs: null
+            method: fit
+    """
+    run("tune", **yaml.safe_load(tune_config), **kwargs)
