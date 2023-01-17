8e65c6e74e279cedcbd3915844a6cf8ef56eb7b0
Ibrahim Hadzic <ibrahimhadzic45@gmail.com>
2023-01-17 13:29:33 -0500

    Reorganize the structure (#13)
    * switch to monai bundle config
    
    * move yaml load function for readability
    
    * user project import not working
    
    * Functional user project importing, but ugly af
    
    * unnecessary import statement
    
    * import user project without yaml imports
    
    * reorganize files, introduce contrib
    
    * Remove contrib
    
    * Add contrib module
    
    * Rename iSSUE_TEMPLATE.md to ISSUE_TEMPLATE.md
    
    * Place contrib into lighter
    
    * rename importing module to dynamic_imports

--- lighter/logger.py
+++ lighter/logger.py
@@ -1,6 +1,11 @@
 from pathlib import Path
+from typing import Dict
 
-from pytorch_lightning.loggers import (LightningLoggerBase, TensorBoardLogger, WandbLogger)
+import torch
+import torchvision
+from loguru import logger
+
+from pytorch_lightning.loggers import LightningLoggerBase, TensorBoardLogger, WandbLogger
 from pytorch_lightning.loggers.logger import rank_zero_experiment
 from pytorch_lightning.utilities import rank_zero_only
 
@@ -83,3 +88,64 @@ class LighterLogger(LightningLoggerBase):
 
         if self.tensorboard_logger is not None:
             self.tensorboard_logger.finalize(status)
+
+
+def preprocess_image(image: torch.Tensor) -> torch.Tensor:
+    """Preprocess the image before logging it. If it is a batch of multiple images,
+    it will create a grid image of them. In case of 3D, a single image is displayed
+    with slices stacked vertically, while a batch as a grid where each column is
+    a different 3D image.
+
+    Args:
+        image (torch.Tensor): 2D or 3D image tensor.
+
+    Returns:
+        torch.Tensor: image ready for logging.
+    """
+    image = image.detach().cpu()
+    # 3D image (NCDHW)
+    has_three_dims = image.ndim == 5
+    if has_three_dims:
+        shape = image.shape
+        image = image.view(shape[0], shape[1], shape[2] * shape[3], shape[4])
+    # If more than one image, create a grid
+    if image.shape[0] > 1:
+        nrow = image.shape[0] if has_three_dims else 8
+        image = torchvision.utils.make_grid(image, nrow=nrow)
+    return image
+
+
+def debug_message(mode: str, input: torch.Tensor, target: torch.Tensor, pred: torch.Tensor,
+                  metrics: Dict, loss: torch.Tensor) -> None:
+    """Logs the debug message.
+
+    Args:
+        mode (str): the mode of the system.
+        input (torch.Tensor): input.
+        target (torch.Tensor): target.
+        pred (torch.Tensor): prediction.
+        metrics (Dict): a dict where keys are the metric names and values the measured values.
+        loss (torch.Tensor): calculated loss.
+    """
+    msg = f"\n----------- Debugging Output -----------\nMode: {mode}"
+    for name, data in {"input": input, "target": target, "pred": pred}.items():
+        if isinstance(data, list):
+            msg += f"\n\n{name.capitalize()} is a {type(data).__name__} of {len(data)} elements."
+            for idx, tensor in enumerate(data):
+                if is_tensor_debug_loggable(tensor):
+                    msg += f"\nTensor {idx} shape and value:\n{tensor.shape}\n{tensor}"
+                else:
+                    msg += f"\n*Tensor {idx} is too big to log."
+        else:
+            if is_tensor_debug_loggable(data):
+                msg += f"\n\n{name.capitalize()} tensor shape and value:\n{data.shape}\n{data}"
+            else:
+                msg += f"\n\n*{name.capitalize()} tensor is too big to log."
+    msg += f"\n\nLoss:\n{loss}"
+    msg += f"\n\nMetrics:\n{metrics}"
+    logger.debug(msg)
+
+
+def is_tensor_debug_loggable(tensor):
+    """A tensor is loggable for debugging if its shape is smaller than 16 in each axis."""
+    return (torch.tensor(tensor.shape[1:]) < 16).all()
---
+++ lighter/utils/model.py
@@ -0,0 +1,67 @@
+import torch
+from torch.nn import Identity, Module, Sequential
+
+from lighter.utils.misc import dot_notation_setattr
+
+
+def reshape_pred_if_single_value_prediction(pred: torch.Tensor,
+                                            target: torch.Tensor) -> torch.Tensor:
+    """When the task is to predict a single value, pred and target dimensions often
+    mismatch - dataloader returns the value in the (B) shape, while the network
+    returns predictions in the (B, 1) shape, where the second dim is redundant.
+
+    Args:
+        pred (torch.Tensor): predicted tensor.
+        target (torch.Tensor): target tensor.
+
+    Returns:
+        torch.Tensor: reshaped predicted tensor if that was necessary.
+    """
+    if isinstance(pred, torch.Tensor) and target is not None:
+        if len(pred.shape) == 2 and len(target.shape) == 1 == pred.shape[1]:
+            return pred.flatten()
+    return pred
+
+
+def replace_layer_with(model: Module, layer_name: str, new_layer: Module) -> Module:
+    """Replaces the specified layer of the network with another layer.
+
+    Args:
+        model (Module): PyTorch model to be edited
+        layer_name (string): Name of the layer which will be replaced.
+            Dot-notation supported, e.g. "layer10.fc".
+
+    Returns:
+        Module: PyTorch model with the new layer set at the specified location.
+    """
+    dot_notation_setattr(model, layer_name, new_layer)
+    return model
+
+
+def replace_layer_with_identity(model: Module, layer_name: str) -> Module:
+    """Replaces any layer of the network with an Identity layer.
+    Useful for removing the last layer of a network to be used as a backbone
+    of an SSL model.
+
+    Args:
+        model (Module): PyTorch model to be edited
+        layer_name (string): Name of the layer which will be replaced with an
+            Identity function. Dot-notation supported, e.g. "layer10.fc".
+
+    Returns:
+        Module: PyTorch model with Identity layer at the specified location.
+    """
+    return replace_layer_with(model, layer_name, Identity())
+
+
+def remove_last_layer_sequentially(model: Module()) -> Sequential:
+    """Removes the last layer of a network and returns it as an nn.Sequential model.
+    Useful when a network is to be used as a backbone of an SSL model.
+
+    Args:
+        model (Module): PyTorch model object.
+
+    Returns:
+        Sequential: PyTorch Sequential model with the last layer removed.
+    """
+    return Sequential(*list(model.children())[:-1])
