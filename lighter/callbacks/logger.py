12742aca39110b25f8e92c2825fd33862e655cf5
Suraj Pai <b.pai@maastrichtuniversity.nl>
2023-02-23 09:54:51 -0500

    Add histogram to logger (#42)
    * Add histogram to logger
    
    * Parity with prediction branch
    
    * Propagate logger code fixes

--- lighter/callbacks/logger.py
+++ lighter/callbacks/logger.py
@@ -12,8 +12,8 @@ from monai.utils.module import optional_import
 from pytorch_lightning import Callback, Trainer
 
 from lighter import LighterSystem
+from lighter.callbacks.utils import check_supported_data_type, get_lighter_mode, parse_data, preprocess_image
 
-LIGHTNING_TO_LIGHTER_STAGE = {"train": "train", "validate": "val", "test": "test"}
 OPTIONAL_IMPORTS = {}
 
 
@@ -133,30 +133,42 @@ class LighterLogger(Callback):
         for data_name in ["input", "target", "pred"]:
             if self.data_types[data_name] is None:
                 continue
+            self._log_by_type(data_name, outputs, mode, step_or_epoch, global_step)
 
-            data_type = self.data_types[data_name]
-            data = outputs[data_name]
-            name = f"{mode}/data/{data_name}/{step_or_epoch}"
+    def _log_by_type(self, data_name: str, outputs: dict, mode: str, step_or_epoch: str, global_step: int) -> None:
+        """Logs the data to TensorBoard and Weights & Biases (if enabled).
+        The data is logged as scalars, images, or histograms, depending on the configuration.
+        """
+        data_type = self.data_types[data_name]
+        data = outputs[data_name]
+        tag = f"{mode}/data/{data_name}/{step_or_epoch}"
 
-            # Scalar
-            if data_type == "scalar":
-                self._log_scalar(name, data, global_step)
+        # Scalar
+        if data_type == "scalar":
+            self._log_scalar(tag, data, global_step)
 
-            # Image
-            elif data_type == "image":
-                # Check if the data type is valid.
-                check_image_data_type(data, data_name)
-                for identifier, image in parse_image_data(data):
-                    item_name = name if identifier is None else f"{name}_{identifier}"
-                    # Slice to `max_samples` only if it less than the batch size.
-                    if self.max_samples is not None and self.max_samples < image.shape[0]:
-                        image = image[: self.max_samples]
-                    # Preprocess a batch of images into a single, loggable, image.
-                    image = preprocess_image(image)
-                    self._log_image(item_name, image, global_step)
-            else:
-                logger.error(f"`{data_name}_type` does not support `{data_type}`.")
-                sys.exit()
+        # Image
+        elif data_type == "image":
+            # Check if the data type is valid.
+            check_supported_data_type(data, data_name)
+            for identifier, image in parse_data(data).items():
+                item_name = tag if identifier is None else f"{tag}_{identifier}"
+                # Slice to `max_samples` only if it less than the batch size.
+                if self.max_samples is not None and self.max_samples < image.shape[0]:
+                    image = image[: self.max_samples]
+                # Preprocess a batch of images into a single, loggable, image.
+                image = preprocess_image(image)
+                self._log_image(item_name, image, global_step)
+
+        # Histogram
+        elif data_type == "histogram":
+            check_supported_data_type(data, data_name)
+            for identifier, tensor in parse_data(data).items():
+                item_name = tag if identifier is None else f"{tag}_{identifier}"
+                self._log_histogram(item_name, tensor, global_step)
+        else:
+            logger.error(f"`{data_name}_type` does not support `{data_type}`.")
+            sys.exit()
 
     def _log_scalar(self, name: str, scalar: Union[int, float, torch.Tensor], global_step: int) -> None:
         """Logs the scalar to TensorBoard and Weights & Biases (if enabled).
@@ -189,6 +201,21 @@ class LighterLogger(Callback):
         if self.wandb:
             self.wandb.log({name: OPTIONAL_IMPORTS["wandb"].Image(image)}, step=global_step)
 
+    def _log_histogram(self, name: str, tensor: torch.Tensor, global_step: int) -> None:
+        """Logs the histogram to TensorBoard and Weights & Biases (if enabled).
+
+        Args:
+            name (str): name of the image to be logged.
+            tensor (torch.Tensor): tensor to be logged.
+            global_step (int): current global step.
+        """
+        tensor = tensor.detach().cpu()
+
+        if self.tensorboard:
+            self.tensorboard.add_histogram(name, tensor, global_step=global_step)
+        if self.wandb:
+            self.wandb.log({name: OPTIONAL_IMPORTS["wandb"].Histogram(tensor)}, step=global_step)
+
     def _on_batch_end(self, outputs: Dict, trainer: Trainer) -> None:
         """Performs logging at the end of a batch/step. It logs the loss and metrics,
         and, if specified how, the input, target, and pred data.
@@ -198,7 +225,7 @@ class LighterLogger(Callback):
             trainer (Trainer): Trainer, passed automatically by PyTorch Lightning.
         """
         if not trainer.sanity_checking:
-            mode = LIGHTNING_TO_LIGHTER_STAGE[trainer.state.stage]
+            mode = get_lighter_mode(trainer.state.stage)
             # Accumulate the loss.
             if mode in ["train", "val"]:
                 self.loss[mode] += outputs["loss"].item()
@@ -221,7 +248,7 @@ class LighterLogger(Callback):
             pl_module (LighterSystem): LighterSystem, passed automatically by PyTorch Lightning.
         """
         if not trainer.sanity_checking:
-            mode = LIGHTNING_TO_LIGHTER_STAGE[trainer.state.stage]
+            mode = get_lighter_mode(trainer.state.stage)
             outputs = {"loss": None, "metrics": None}
 
             # Loss
@@ -264,7 +291,7 @@ class LighterLogger(Callback):
         Returns:
             int: global step.
         """
-        mode = LIGHTNING_TO_LIGHTER_STAGE[trainer.state.stage]
+        mode = get_lighter_mode(trainer.state.stage)
         # When validating in Trainer.fit(), return the train steps instead of the
         # val steps to correctly
         if mode == "val" and trainer.state.fn == "fit":
@@ -302,89 +329,3 @@ class LighterLogger(Callback):
 
     def on_test_epoch_end(self, trainer: Trainer, pl_module: LighterSystem) -> None:
         self._on_epoch_end(trainer, pl_module)
-
-
-def preprocess_image(image: torch.Tensor) -> torch.Tensor:
-    """Preprocess the image before logging it. If it is a batch of multiple images,
-    it will create a grid image of them. In case of 3D, a single image is displayed
-    with slices stacked vertically, while a batch as a grid where each column is
-    a different 3D image.
-    Args:
-        image (torch.Tensor): 2D or 3D image tensor.
-    Returns:
-        torch.Tensor: image ready for logging.
-    """
-    image = image.detach().cpu()
-    # 3D image (NCDHW)
-    has_three_dims = image.ndim == 5
-    if has_three_dims:
-        # Reshape 3D image from NCDHW to NC(D*H)W format
-        shape = image.shape
-        image = image.view(shape[0], shape[1], shape[2] * shape[3], shape[4])
-    if image.shape[0] == 1:
-        image = image[0]
-    else:
-        # If more than one image, create a grid
-        nrow = image.shape[0] if has_three_dims else 8
-        image = torchvision.utils.make_grid(image, nrow=nrow)
-    return image
-
-
-def check_image_data_type(data: Any, name: str) -> None:
-    """Check the input image data for its type. Valid image data types are:
-        - torch.Tensor
-        - List[torch.Tensor]
-        - Dict[str, torch.Tensor]
-        - Dict[str, List[torch.Tensor]]
-
-    Args:
-        data (Any): image data to check
-        name (str): name of the image data, for logging purposes.
-    """
-    if isinstance(data, dict):
-        is_valid = all(check_image_data_type(elem, name) for elem in data.values())
-    elif isinstance(data, list):
-        is_valid = all(check_image_data_type(elem, name) for elem in data)
-    elif isinstance(data, torch.Tensor):
-        is_valid = True
-    else:
-        is_valid = False
-
-    if not is_valid:
-        logger.error(
-            f"`{name}` has to be a Tensor, List[Tensor], Dict[str, Tensor]"
-            f", or Dict[str, List[Tensor]]. `{type(data)}` is not supported."
-        )
-        sys.exit()
-
-    return is_valid
-
-
-def parse_image_data(
-    data: Union[Dict[str, torch.Tensor], Dict[str, List[torch.Tensor]], List[torch.Tensor], torch.Tensor]
-) -> List[Tuple[Optional[str], torch.Tensor]]:
-    """Given input data, this function will parse it and return a list of tuples where
-    each tuple contains an identifier and a tensor.
-
-    Args:
-        data (Union[torch.Tensor, List[torch.Tensor], Dict[str, torch.Tensor],
-                    Dict[str, List[torch.Tensor]]]): image tensor(s).
-
-    Returns:
-        List[Tuple[Optional[str], torch.Tensor]]: a list of tuples where the first element is
-            a string identifier (or `None` if there is only one image) and the second an image tensor.
-    """
-    result = []
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(value, list):
-                for i, tensor in enumerate(value):
-                    result.append((f"{key}_{i}", tensor) if len(value) > 1 else (key, tensor))
-            else:
-                result.append((key, value))
-    elif isinstance(data, list):
-        for i, tensor in enumerate(data):
-            result.append((str(i), tensor))
-    else:
-        result.append((None, data))
-    return result
