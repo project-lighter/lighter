7698cd13546997799d3c36a5925f388830d2fb36
Suraj Pai <b.pai@maastrichtuniversity.nl>
2023-04-14 10:30:32 -0400

    Simplify config instantiation (#57)
    * Update poetry - PL2.0 and torch2.0
    
    * Simplify lighter cli
    
    * Replace assert with raise
    
    * Fix tests and improve config parsing
    
    * Google-style docstrings

--- tests/integration/test_cifar.py
+++ tests/integration/test_cifar.py
@@ -7,21 +7,19 @@ test_overrides = "./tests/integration/test_overrides.yaml"
 
 
 @pytest.mark.parametrize(
-    ("method_name", "method_config", "config_file"),
+    ("method_name", "config_file"),
     [
         (  # Method name
             "fit",
-            # Method config
-            {"fit": {"_target_": "$@trainer.fit", "model": "@system"}},
             # Config fiile
             "./projects/cifar10/experiments/monai_bundle_prototype.yaml",
         )
     ],
 )
 @pytest.mark.slow
-def test_trainer_method(method_name: str, method_config: dict, config_file: str):
+def test_trainer_method(method_name: str, config_file: str):
     """ """
     kwargs = {"config_file": config_file, "args_file": test_overrides}
 
-    func_return = run_trainer_method(method_name, method_config, **kwargs)
+    func_return = run_trainer_method(method_name, **kwargs)
     assert func_return is None
