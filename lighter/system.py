c9734da2dffdf4fa4802fc416a18e461f96e09cf
Ibrahim Hadzic <ibrahimhadzic45@gmail.com>
2023-06-02 20:06:35 -0400

    Disable sync_dist in system logging

--- lighter/system.py
+++ lighter/system.py
@@ -232,8 +232,7 @@ class LighterSystem(pl.LightningModule):
             loss = self._calculate_loss(pred, target)
             loss_name = "loss" if mode == "train" else f"{mode}_loss"
             # Log the loss and for monitoring purposes.
-            self.log(loss_name, loss, on_step=True, on_epoch=False, sync_dist=False, logger=False, batch_size=self.batch_size)
-            self.log(loss_name, loss, on_step=False, on_epoch=True, sync_dist=True, logger=False, batch_size=self.batch_size)
+            self.log(loss_name, loss, on_step=True, on_epoch=True, sync_dist=False, logger=False, batch_size=self.batch_size)
 
         # Apply the post-criterion activation. Necessary for measuring the metrics
         # correctly in cases when using a criterion such as `BCELossWithLogits`` which
@@ -248,8 +247,7 @@ class LighterSystem(pl.LightningModule):
             # Calculate the metrics for the step.
             metrics = getattr(self, f"{mode}_metrics")(pred, target)
             # Log the metrics for monitoring purposes.
-            self.log_dict(metrics, on_step=True, on_epoch=False, sync_dist=False, logger=False, batch_size=self.batch_size)
-            self.log_dict(metrics, on_step=False, on_epoch=True, sync_dist=True, logger=False, batch_size=self.batch_size)
+            self.log_dict(metrics, on_step=True, on_epoch=True, sync_dist=False, logger=False, batch_size=self.batch_size)
             # Return the loss, metrics, input, target, and pred.
             return {"loss": loss, "metrics": metrics, "input": input, "target": target, "pred": pred}
 
