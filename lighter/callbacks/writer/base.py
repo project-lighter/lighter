Fix index ID issue in DDP writing. Replace broadcast with gather in the TableWriter.

lighter/callbacks/writer/base.py
@@ -43,8 +43,8 @@ class LighterBaseWriter(ABC, Callback):
         # Placeholder for processed format for quicker access during writes
         self._parsed_format = None
 
-        # Keeps track of last written prediction index for cases when ids aren't provided
-        self._current_pred_index = 0
+        # When IDs are not provided, keep track of the global prediction count. Supports DDP.
+        self._pred_count = None
 
         # Ensure that default writers are defined
         if not hasattr(self, "_writers"):
@@ -86,10 +86,17 @@ class LighterBaseWriter(ABC, Callback):
         if stage != "predict":
             return
 
+        # Initialize the prediction count with the rank of the current process
+        self._pred_count = torch.distributed.get_rank()
+
         # Ensure all distributed nodes write to the same directory
-        self.directory = trainer.strategy.broadcast(self.directory)
+        self.directory = trainer.strategy.broadcast(self.directory, src=0)
         if trainer.is_global_zero:
             self.directory.mkdir(parents=True)
+        # Wait for rank 0 to create the directory
+        trainer.strategy.barrier()
+
+        # Ensure all distributed nodes have access to the directory
         if not self.directory.exists():
             raise RuntimeError(
                 f"Rank {trainer.global_rank} does not share storage with rank 0. Ensure nodes have common storage access."
@@ -104,10 +111,15 @@ class LighterBaseWriter(ABC, Callback):
         # Fetch and decollate IDs if provided.
         if outputs["id"] is not None:
             ids = decollate_batch(outputs["id"], detach=True, pad=False)
-        # Generate IDs if not provided. An ID will be the index of the prediction.
+        # Generate IDs if not provided. An ID will be the global index of the prediction.
         else:
-            ids = list(range(self._current_pred_index, self._current_pred_index + len(preds)))
-            self._current_pred_index += len(preds)
+            ids = []
+            for _ in range(len(preds)):
+                # Append the current prediction count to the IDs list.
+                ids.append(self._pred_count)
+                # Increment the prediction count by the total number of DDP processes.
+                # This ensures each process will generate unique IDs in the next batch.
+                self._pred_count += trainer.world_size
 
         # Iterate over the predictions and save them.
         for id, pred in zip(ids, preds):

lighter/callbacks/writer/table.py
@@ -4,6 +4,7 @@ import itertools
 from pathlib import Path
 
 import pandas as pd
+import torch
 from pytorch_lightning import Trainer
 
 from lighter import LighterSystem
@@ -80,17 +81,21 @@ class LighterTableWriter(LighterBaseWriter):
         csv_path = self.directory / "predictions.csv"
 
         # Sort the records by ID and convert the dictionary to a list
-        self.csv_records = [self.csv_records[key] for key in sorted(self.csv_records)]
+        self.csv_records = [self.csv_records[id] for id in sorted(self.csv_records)]
 
-        # If in distributed data parallel mode, gather records from all processes
+        # If in distributed data parallel mode, gather records from all processes to rank 0.
         if trainer.world_size > 1:
-            ddp_csv_records = [self.csv_records] * trainer.world_size
-            for rank in range(trainer.world_size):
-                ddp_csv_records[rank] = trainer.strategy.broadcast(ddp_csv_records[rank], src=rank)
-            self.csv_records = list(itertools.chain(*ddp_csv_records))
+            # Create a list to hold the records from each process. Used on rank 0 only.
+            gather_csv_records = [None] * trainer.world_size if trainer.is_global_zero else None
+            # Each process sends its records to rank 0, which stores them in the `gather_csv_records`.
+            torch.distributed.gather_object(self.csv_records, gather_csv_records, dst=0)
+            # Concatenate the gathered records
+            if trainer.is_global_zero:
+                self.csv_records = list(itertools.chain(*gather_csv_records))
 
-        # Convert the list of records to a dataframe and save it as a CSV file
-        pd.DataFrame(self.csv_records).to_csv(csv_path)
+        # Save the records to a CSV file
+        if trainer.is_global_zero:
+            pd.DataFrame(self.csv_records).to_csv(csv_path)
 
 
 def write_tensor(tensor: Any) -> List: