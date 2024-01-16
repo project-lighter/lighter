12c1bb9763be52954771c0d4c208f0c7f1a6f3af
Suraj Pai <b.pai@maastrichtuniversity.nl>
2024-01-16 18:44:35 -0500

    Refactor logging (#93)
    * Add loss and metrics logging as PL does it
    
    * Add optimizer stats logging
    
    * Remove LighterLogger
    
    * Add config logging to loggers and checkpoints. Remove args_file.
    
    * Fix attempts to log when no logger is present. Update CIFAR10
    
    ---------
    
    Co-authored-by: Ibrahim Hadzic <ibrahimhadzic45@gmail.com>

--- lighter/utils/misc.py
+++ lighter/utils/misc.py
@@ -2,6 +2,8 @@ from typing import Any, Callable, Dict, List, Union
 
 import inspect
 
+from torch.optim.optimizer import Optimizer
+
 
 def ensure_list(vals: Any) -> List:
     """Wrap the input into a list if it is not a list. If it is a None, return an empty list.
@@ -114,3 +116,35 @@ def apply_fns(data: Any, fns: Union[Callable, List[Callable]]) -> Any:
     for fn in ensure_list(fns):
         data = fn(data)
     return data
+
+
+def get_optimizer_stats(optimizer: Optimizer) -> Dict[str, float]:
+    """
+    Extract learning rates and momentum values from each parameter group of the optimizer.
+
+    Args:
+        optimizer (Optimizer): A PyTorch optimizer.
+
+    Returns:
+        Dict[str, float]: Dictionary with formatted keys and values for learning rates and momentum.
+    """
+    stats_dict = {}
+    for group_idx, group in enumerate(optimizer.param_groups):
+        lr_key = f"optimizer/{optimizer.__class__.__name__}/lr"
+        momentum_key = f"optimizer/{optimizer.__class__.__name__}/momentum"
+
+        # Add group index to the key if there are multiple parameter groups
+        if len(optimizer.param_groups) > 1:
+            lr_key += f"/group{group_idx+1}"
+            momentum_key += f"/group{group_idx+1}"
+
+        # Extracting learning rate
+        stats_dict[lr_key] = group["lr"]
+
+        # Extracting momentum or betas[0] if available
+        if "momentum" in group:
+            stats_dict[momentum_key] = group["momentum"]
+        if "betas" in group:
+            stats_dict[momentum_key] = group["betas"][0]
+
+    return stats_dict
