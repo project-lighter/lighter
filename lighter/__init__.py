3082611b4038312e6779315bbdd1f76db4c89abd
Suraj Pai <bspai@bwh.harvard.edu>
2025-01-21 12:13:05 -0500

    Poetry >uv (#144)
    * Latest package versions
    
    * Migrate from poetry to uv
    
    * Revert version
    
    * Fix style issues
    
    * Rename CI
    
    * Fix CI pipeline
    
    * Remove 3.9 support
    
    * Fix bumping
    
    * Fix codestyle (again)
    
    * Simplify tests
    
    * Fix style
    
    * Upgrade MONAI version

--- lighter/__init__.py
+++ lighter/__init__.py
@@ -2,10 +2,10 @@
 Lighter is a framework for streamlining deep learning experiments with configuration files.
 """
 
+from .system import LighterSystem
+
 __version__ = "0.0.3a11"
 
 from .utils.logging import _setup_logging
 
 _setup_logging()
-
-from .system import LighterSystem
