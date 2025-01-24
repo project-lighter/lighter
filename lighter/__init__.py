7e1ad1c1fce0bf7b9512b52eba9e3507fb0afe82
Suraj Pai <bspai@bwh.harvard.edu>
2025-01-24 17:09:55 -0500

    Fix init import

--- lighter/__init__.py
+++ lighter/__init__.py
@@ -1,9 +1,6 @@
 """
 Lighter is a framework for streamlining deep learning experiments with configuration files.
 """
-
-from .system import LighterSystem
-
 __version__ = "0.0.3a12"
 
 from .utils.logging import _setup_logging
