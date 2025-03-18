9271e392bdc58bed0994b87bc8c5ba1a705bf307
GitHub Action <action@github.com>
2025-03-18 20:00:01 +0000

    Bump version

--- lighter/__init__.py
+++ lighter/__init__.py
@@ -2,7 +2,7 @@
 Lighter is a framework for streamlining deep learning experiments with configuration files.
 """
 
-__version__ = "0.0.3a21"
+__version__ = "0.0.3a22"
 
 from .utils.logging import _setup_logging
 
